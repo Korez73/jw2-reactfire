ac5fdbe946a1c2916557029fb8aa8b864c166c39
Jamie Weldon <jamie.weldon@outlook.com>
2024-08-20 15:57:23 -0500

    add props to the card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 import './App.css';
 
+const photos = [
+  'https://picsum.photos/id/1001/200/200', 
+  'https://picsum.photos/id/1002/200/200', 
+  'https://picsum.photos/id/1003/200/200', 
+  'https://picsum.photos/id/1004/200/200', 
+  'https://picsum.photos/id/1005/200/200', 
+  'https://picsum.photos/id/1006/200/200'
+]
+
 function App() {
   return (
     <>
@@ -10,7 +19,7 @@ function App() {
     <div className="container text-center mt-5">
       <h1>Gallery</h1>
       <div className="row">
-        {Array.apply(null, {length: 9}).map(() => <Card />)}
+        {photos.map((photo) => <Card src={photo} />)}
       </div>
     </div>
     </>
--- src/components/Navsearch.js
+++ src/components/Navsearch.js
@@ -1,8 +1,8 @@
-function Card(props) {
+function Navsearch(props) {
   return(
       <div className="card" style={{width: "18rem"}}>
        <img src={props.src} className="card-img-top" alt={props.src} />
       </div>
   )
 }
-export default Card;
+export default Navsearch;
