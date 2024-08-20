1eed3fb970419d361742bfca35f659129acdd088
Jamie Weldon <jamie.weldon@outlook.com>
2024-08-20 17:13:10 -0500

    move navbar code into seperate components

--- src/components/Navsearch.js
+++
@@ -1,8 +0,0 @@
-function Navsearch(props) {
-  return(
-      <div className="card" style={{width: "18rem"}}>
-       <img src={props.src} className="card-img-top" alt={props.src} />
-      </div>
-  )
-}
-export default Navsearch;
