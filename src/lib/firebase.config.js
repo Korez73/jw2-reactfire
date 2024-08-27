52e74b94b56b5222cd86d3fcced823bd691538c5
Jamie Weldon <jamie.weldon@outlook.com>
2024-08-27 15:13:37 -0500

    define environment variables

--- src/lib/firebase.config.js
+++ src/lib/firebase.config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAq2Wit9s1ezrxDnWFTGYDAT8mCSF5v0a0",
-  authDomain: "jwreactfire.firebaseapp.com",
-  projectId: "jwreactfire",
-  storageBucket: "jwreactfire.appspot.com",
-  messagingSenderId: "900430226059",
-  appId: "1:900430226059:web:c5d13581268282a31dea8e"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
