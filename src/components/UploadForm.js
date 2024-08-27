348585b307ad2235978dfb2012c3e8d96f91f004
Jamie Weldon <jamie.weldon@outlook.com>
2024-08-27 05:07:18 -0500

    add writeDoc to firestore

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,38 +1,41 @@
-import { useMemo, useContext } from "react";
+import { useMemo,  useContext } from "react"
+import { Context } from "../context"
+import Firestore from "../handlers/firestore";
 
-import { Context } from "../context";
+const { writeDoc } = Firestore
 
 const Preview = () => {
-  const {state} = useContext(Context);
-  const {inputs } = state;
+  const { state } = useContext(Context)
+  const { inputs : { path } } = state  // destructuring the current state
   return (
-    inputs.path &&
+    path &&
     <div
       className="rounded p-1 m-5"
       style={{
         width: "30%",
         height: "300px",
-        backgroundImage: `url(${inputs.path}`,
+        backgroundImage: `url(${path}`,
         backgroundSize: "cover",
       }}
     ></div>
   );
 };
 
-
 const UploadForm = () => {
-  const {dispatch, state} = useContext(Context);
-  const isDisabled = useMemo(() => {
-    return !!Object.values(state.inputs).some(input => !input);
-  }, [state.inputs]);
-  const handleOnChange = (e) => dispatch({type: 'setInputs', payload: {value: e}})
+  const { dispatch, state } = useContext(Context)
+  const { isCollapsed : isVisible, inputs  } = state // destructuring the current state
+  const handleOnChange = (e) => dispatch({ type: 'setInputs', payload: { value: e}})
   const handleOnSubmit = (e) => {
     e.preventDefault() 
+    writeDoc(inputs, "stocks").then(console.log)
     dispatch({ type: 'setItem'})
-    dispatch({type: "collapse", payload: { bool: false }})
+    dispatch({ type: "collapse", payload: { bool: false }})
   }  
+  const isDisabled = useMemo(() => {
+    return !!Object.values(inputs).some(input => !input)
+  }, [inputs])
   return (
-    state.isCollapsed && <>
+    isVisible && <>
       <p className="display-6 text-center mb-3">Upload Stock Image</p>
       <div className="mb-5 d-flex align-items-center justify-content-center">
         <Preview />
@@ -48,7 +51,7 @@ const UploadForm = () => {
             />
           </div>
           <div className="mb-3">
-            <input type="file" className="form-control" name="file" onChange={handleOnChange} />
+            <input type="file" className="form-control" name="file" onChange={handleOnChange}/>
           </div>
           <button
             type="submit"
@@ -62,5 +65,4 @@ const UploadForm = () => {
     </>
   );
 };
-
 export default UploadForm;
--- src/lib/firebase.config.js
+++ src/lib/firebase.config.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,15 +13,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-//const app = initializeApp(firebaseConfig);
-const app = () => {
-  if(!firebaseConfig || !firebaseConfig.apiKey) {
-    throw new Error('No Firebase configuration object provided.' + '\n' + 
-      'Add your web app\'s configuration object to firebase.config.js');
-  } else {
-    console.log('Firebase initialized!');
-  }
-  return initializeApp(firebaseConfig);
-}
-
+const app = initializeApp(firebaseConfig);
+export const db  = getFirestore(app);
 export default app;
