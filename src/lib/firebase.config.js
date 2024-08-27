ee45b218680c69b3a153e06022883c19070040b4
Jamie Weldon <jamie.weldon@outlook.com>
2024-08-27 15:41:38 -0500

    Define an async function to send a file to storage

--- src/lib/firebase.config.js
+++ src/lib/firebase.config.js
@@ -1,6 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import {  getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,6 +14,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db  = getFirestore(app);
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
 export default app;
