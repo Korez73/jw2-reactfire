618b3574b148429eafb3544293ea8516a8dbfabb
Jamie Weldon <jamie.weldon@outlook.com>
2024-09-03 05:55:24 -0500

    Manage the state with the Context API: Custom hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,26 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { useAuthContext } from "../context/AuthContext"
+
+const LogIn = () => {
+  const {login, currentUser } = useAuthContext()
+  return (
+    !currentUser && 
+    <button type="button" className="btn btn-warning" onClick={login}>
+      Login
+    </button>
+  );
+};
+
+const LogOut = () => {
+  const {logout, currentUser } = useAuthContext()
+  return (
+    !!currentUser && 
+    <button type="button" className="btn btn-danger" onClick={logout}>
+      Logout
+    </button>
+  );
+ };
+
 function Navigation() {
   return(
     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -43,12 +65,17 @@ function Dropdown() {
     >
       Login
     </a>
-    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+    <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
       <li>
         <a className="dropdown-item text-center" href="#">
           Profile
         </a>
+        <li><hr className="dropdown divider"/></li>
       </li>
+      <div className="d-flex justify-content-center">
+        <LogIn />
+        <LogOut />
+      </div>
     </ul>
   </li>
 </ul>)
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,24 +1,28 @@
-import { createContext, useMemo, useState } from "react"
+import { createContext, useContext, useMemo, useState } from "react"
 import FirebaseAuth from "../handlers/auth"
 
 
-const { signIn, signOut }= FirebaseAuth
+const { signIn, signOut, getCurrentUser }= FirebaseAuth
 const Context = createContext()
 
 
 const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null)
 
-  const login = () => {}
-  const logout = () => {}
+  const login = () => signIn().then(setCurrentUser)
+  const logout = () => signOut().then(() => setCurrentUser(null))
   const value = useMemo(() => {
     return {
       login,
       logout,
       currentUser
     }
-  }, [])
+  }, [login, logout, currentUser])
   return <Context.Provider value={value}>{children}</Context.Provider>
 }
 
+
+export const useAuthContext = () => {
+  return useContext(Context)
+}
 export default AuthProvider
