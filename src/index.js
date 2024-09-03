c6d2859217705c70a893fa7d8b4da4bc2c7c1c4c
Jamie Weldon <jamie.weldon@outlook.com>
2024-09-03 15:37:26 -0500

    Add a beginner route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Provider from './context/FirestoreContext';
 import AuthProvider from './context/AuthContext';
+
+function Stocks() {
+  return <h1>My Stock Images</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +17,10 @@ root.render(
     <AuthProvider>
       <Provider>
         <Router>
-          <App />
+          <Routes>
+            <Route path="/" element={<App />}/>
+            <Route path="/stocks" element={<Stocks />}/>
+          </Routes>
         </Router>
       </Provider>
     </AuthProvider>
