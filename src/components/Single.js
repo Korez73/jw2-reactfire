0b1fcabec3098aaabd9613b1c5fed2b6f3911f52
Jamie Weldon <jamie.weldon@outlook.com>
2024-09-05 07:31:09 -0500

    complete the single card information and allow the user to go back

--- src/components/Single.js
+++ src/components/Single.js
@@ -2,12 +2,16 @@ import { useLocation, useNavigate } from "react-router-dom"
 import { useFirestoreContext } from "../context/FirestoreContext"
 import Card from "./Card"
 
-const Single = () => {
+function Single() {
+    const navigate = useNavigate()
+    const { state } = useFirestoreContext()
+    const { state: routerState } = useLocation()
+    const item = state.items.find(item => item.id === routerState.id)
     return(
        <>
-        <button className="btn btn-link" onClick={() => {}}>Back</button>
+        <button className="btn btn-link" onClick={() => navigate(-1)}>Back</button>
         <div className="d-flex justify-content-center mb-5">
-            <Card  />
+            <Card  {...item}/>
         </div>
        </>
     )
